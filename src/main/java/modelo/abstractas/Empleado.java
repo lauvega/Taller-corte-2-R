f78d8ab39f44f9f9cf6013982afa9c6ad4323922
lauvega <lauravegat2008@gmail.com>
2026-04-03 14:57:55 -0500

    Taller 2 del corte 1 sistemaBancario actualizando

--- src/main/java/modelo/abstractas/Empleado.java
+++ src/main/java/modelo/abstractas/Empleado.java
@@ -1,13 +1,38 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package modelo.abstractas;
 
-/**
- *
- * @author laura
- */
-public class Empleado {
-    
+import java.time.LocalDate;
+import java.time.Period;
+
+public abstract class Empleado extends Persona {
+
+    private String legajo;
+    private LocalDate fechaContratacion;
+    protected double salarioBase;
+    private boolean activo;
+
+    public Empleado(String id, String nombre, String apellido,
+                    LocalDate fechaNacimiento, String email,
+                    String legajo, LocalDate fechaContratacion,
+                    double salarioBase) {
+
+        super(id, nombre, apellido, fechaNacimiento, email);
+
+        this.legajo = legajo;
+        this.fechaContratacion = fechaContratacion;
+        this.salarioBase = salarioBase;
+        this.activo = true;
+    }
+
+    //  Metodos Abstractatos
+    public abstract double calcularSalario();
+    public abstract double calcularBono();
+
+    // Metodo concreto
+    public int calcularAntiguedad() {
+        return Period.between(fechaContratacion, LocalDate.now()).getYears();
+    }
+
+    public boolean isActivo() {
+        return activo;
+    }
 }
--- src/main/java/modelo/abstractas/Persona.java
+++ src/main/java/modelo/abstractas/Persona.java
@@ -1,13 +1,68 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package modelo.abstractas;
 
-/**
- *
- * @author laura
- */
-public class Persona {
-    
+import modelo.excepciones.DatoInvalidoException;
+import java.time.LocalDate;
+import java.time.Period;
+
+public abstract class Persona {
+
+    private String id;
+    private String nombre;
+    private String apellido;
+    private LocalDate fechaNacimiento;
+    private String email;
+
+    public Persona(String id, String nombre, String apellido,
+                   LocalDate fechaNacimiento, String email) {
+
+        setId(id);
+        this.nombre = nombre;
+        this.apellido = apellido;
+        setFechaNacimiento(fechaNacimiento);
+        setEmail(email);
+    }
+
+  
+    public abstract int calcularEdad();
+    public abstract String obtenerTipo();
+    public abstract String obtenerDocumentoIdentidad();
+
+
+    public String getNombreCompleto() {
+        return nombre + " " + apellido;
+    }
+
+
+    public void setId(String id) {
+        if (id == null || id.isEmpty()) {
+            throw new DatoInvalidoException("id", id);
+        }
+        this.id = id;
+    }
+
+    public void setEmail(String email) {
+        if (email == null || !email.contains("@")) {
+            throw new DatoInvalidoException("email", email);
+        }
+        this.email = email;
+    }
+
+    public void setFechaNacimiento(LocalDate fecha) {
+        if (fecha.isAfter(LocalDate.now())) {
+            throw new DatoInvalidoException("fechaNacimiento", fecha);
+        }
+        this.fechaNacimiento = fecha;
+    }
+
+    public int calcularEdadBase() {
+        return Period.between(fechaNacimiento, LocalDate.now()).getYears();
+    }
+
+    public String getId() {
+        return id;
+    }
 }
+
+
+
